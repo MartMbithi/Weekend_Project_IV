f931d4d11c0619df230e37e318c7952d1b382d73
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-08 15:48:53 +0300

    Register .htaccess Conf File

--- index.php
+++ index.php
@@ -4,3 +4,12 @@
  *
  * Copyright (c) 2022 MartMbithi
  */
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+/* Redirect To Index Under Views */
+header('Location: ' . $uri . '/ui/login');
+exit;
