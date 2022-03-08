fbb0c9e4f8e586802039f99ba0fe74dd84f09333
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-08 19:10:30 +0300

    Implement Side Navigation Bar

--- app/partials/aside.php
+++ app/partials/aside.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="dashboard" class="brand-link">
+        <img src="../assets/upload/logo.png" alt="" class="brand-image img-circle elevation-4 border-success">
+        <span class="brand-text font-weight-light">House Rental MIS</span>
     </a>
 
     <!-- Sidebar -->
