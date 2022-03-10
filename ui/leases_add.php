ea92fa3fcc5753332312effcb75a5cb51258549f
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-10 13:54:22 +0300

    Implement Properties Add

--- ui/leases_add.php
+++ ui/leases_add.php
@@ -1,5 +1,5 @@
 <?php
-/* Add Property */
+/* Leases Add */
 require_once('../app/partials/head.php');
 ?>
 
@@ -19,13 +19,13 @@ require_once('../app/partials/head.php');
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-8">
-                            <h1 class="m-0 text-dark">Property Management Module - Register Property</h1>
+                            <h1 class="m-0 text-dark">Property Leases Module - Add Lease</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-4">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
-                                <li class="breadcrumb-item"><a href="">Properties</a></li>
-                                <li class="breadcrumb-item active">Register</li>
+                                <li class="breadcrumb-item"><a href="">Property Leases</a></li>
+                                <li class="breadcrumb-item active">Add</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -44,22 +44,33 @@ require_once('../app/partials/head.php');
                                 <div class="card-body">
                                     <form method="post" enctype="multipart/form-data" role="form">
                                         <div class="row">
-                                            <div class="form-group col-md-8">
-                                                <label for="">Property Name</label>
-                                                <input type="text" required name="property_name" class="form-control">
+                                            <div class="form-group col-md-12">
+                                                <label for="">Property Details</label>
+                                                <select class="form-control basic" name="lease_property_id">
+                                                    <option>Select Property</option>
+                                                </select>
                                             </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="">Property Code</label>
-                                                <input type="text" readonly required name="property_code" class="form-control">
+                                            <div class="form-group col-md-12">
+                                                <label for="">Tenant Details</label>
+                                                <select class="form-control basic" name="lease_tenant_id">
+                                                    <option>Select Tenant</option>
+                                                </select>
                                             </div>
-                                            <div class="form-group col-md-2">
-                                                <label for="">Property Monthly Rent</label>
-                                                <input type="text" required name="property_cost" class="form-control">
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <label for="">Property Category</label>
-                                                <select class="form-control basic" name="property_category_id">
-                                                    <option>Select Category</option>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Lease Duration (Months)</label>
+                                                <select class="form-control basic" name="lease_duration">
+                                                    <option>1</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                    <option>6</option>
+                                                    <option>7</option>
+                                                    <option>8</option>
+                                                    <option>9</option>
+                                                    <option>10</option>
+                                                    <option>11</option>
+                                                    <option>12</option>
                                                 </select>
                                             </div>
                                             <div class="form-group col-md-6">
