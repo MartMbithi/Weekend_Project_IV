b570349f1b22a45e7ef962b3598e876a64108618
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-11 17:26:29 +0300

    Implement Landlords  Update And Delete Front Ends

--- ui/users_landlords.php
+++ ui/users_landlords.php
@@ -143,93 +143,99 @@ require_once('../app/partials/head.php');
                                             </tr>
                                         </thead>
                                         <tbody>
-
-                                            <tr>
-                                                <td>Name</td>
-                                                <td>IDNO</td>
-                                                <td>Email</td>
-                                                <td>Phone</td>
-                                                <td>Address</td>
-                                                <td>
-                                                    <a data-toggle="modal" href="#update_" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a>
-                                                    <a data-toggle="modal" href="#delete_" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
-                                                </td>
-                                                <!-- Update Modal -->
-                                                <div class="modal fade fixed-right" id="update_" tabindex="-1" role="dialog" aria-hidden="true">
-                                                    <div class="modal-dialog  modal-xl" role="document">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header align-items-center">
-                                                                <div class="text-bold">
-                                                                    <h6 class="text-bold">Update</h6>
-                                                                </div>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <form method="post" enctype="multipart/form-data" role="form">
-                                                                    <div class="row">
-                                                                        <div class="form-group col-md-12">
-                                                                            <label for="">Full Name</label>
-                                                                            <input type="text" required name="user_name" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
-                                                                        <div class="form-group col-md-4">
-                                                                            <label for="">National ID Number</label>
-                                                                            <input type="text" required name="user_idno" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
-                                                                        <div class="form-group col-md-4">
-                                                                            <label for="">Access Rights</label>
-                                                                            <input type="text" readonly required name="user_access_level" value="landlord" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
-                                                                        <div class="form-group col-md-4">
-                                                                            <label for="">Phone Number</label>
-                                                                            <input type="text" required name="user_phoneno" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
-                                                                        <div class="form-group col-md-6">
-                                                                            <label for="">Email Address</label>
-                                                                            <input type="text" name="user_email" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
-                                                                        <div class="form-group col-md-6">
-                                                                            <label for="">Address</label>
-                                                                            <input type="text" name="user_address" class="form-control" id="exampleInputEmail1">
-                                                                        </div>
+                                            <?php
+                                            $ret = "SELECT * FROM users WHERE user_access_level = 'landlord' ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($landlords = $res->fetch_object()) {
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $landlords->user_name; ?></td>
+                                                    <td><?php echo $landlords->user_idno; ?></td>
+                                                    <td><?php echo $landlords->user_email; ?></td>
+                                                    <td><?php echo $landlords->user_phoneno; ?></td>
+                                                    <td><?php echo $landlords->user_address; ?></td>
+                                                    <td>
+                                                        <a data-toggle="modal" href="#update_<?php echo $landlords->user_id; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a>
+                                                        <a data-toggle="modal" href="#delete_<?php echo $landlords->user_id; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
+                                                    </td>
+                                                    <!-- Update Modal -->
+                                                    <div class="modal fade fixed-right" id="update_<?php echo $landlords->user_id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+                                                        <div class="modal-dialog  modal-xl" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header align-items-center">
+                                                                    <div class="text-bold">
+                                                                        <h6 class="text-bold">Update</h6>
                                                                     </div>
-                                                                    <div class="text-right">
-                                                                        <button type="submit" name="update_landlord" class="btn btn-warning">Update Landlord</button>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <form method="post" enctype="multipart/form-data" role="form">
+                                                                        <div class="row">
+                                                                            <div class="form-group col-md-12">
+                                                                                <label for="">Full Name</label>
+                                                                                <input type="text" required name="user_name" value="<?php echo $landlords->user_name; ?>" class="form-control" id="exampleInputEmail1">
+                                                                            </div>
+                                                                            <div class="form-group col-md-4">
+                                                                                <label for="">National ID Number</label>
+                                                                                <input type="text" required name="user_idno" value="<?php echo $landlords->user_idno; ?>" class="form-control" id="exampleInputEmail1">
+                                                                            </div>
+                                                                            <div class="form-group col-md-4">
+                                                                                <label for="">Access Rights</label>
+                                                                                <input type="text" readonly required name="user_access_level" value="landlord" class="form-control" id="exampleInputEmail1">
+                                                                            </div>
+                                                                            <div class="form-group col-md-4">
+                                                                                <label for="">Phone Number</label>
+                                                                                <input type="text" required name="user_phoneno" value="<?php echo $landlords->user_phoneno; ?>" class="form-control" id="exampleInputEmail1">
+                                                                            </div>
+                                                                            <div class="form-group col-md-6">
+                                                                                <label for="">Email Address</label>
+                                                                                <input type="text" name="user_email" class="form-control" value="<?php echo $landlords->user_email; ?>" id="exampleInputEmail1">
+                                                                            </div>
+                                                                            <div class="form-group col-md-6">
+                                                                                <label for="">Address</label>
+                                                                                <input type="text" name="user_address" value="<?php echo $landlords->user_address; ?>" class="form-control" id="exampleInputEmail1">
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="text-right">
+                                                                            <button type="submit" name="update_landlord" class="btn btn-warning">Update Landlord</button>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- End Modal -->
+
+                                                    <!-- Delete Modal -->
+                                                    <div class="modal fade" id="delete_<?php echo $landlords->user_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
+                                                                    <button type="button" class="close" data-dismiss="modal">
+                                                                        <span>&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <form method="POST">
+                                                                    <div class="modal-body text-center text-danger">
+                                                                        <h4>Delete <?php echo $landlords->user_name; ?> </h4>
+                                                                        <br>
+                                                                        <!-- Hide This -->
+                                                                        <input type="hidden" name="user_id" value="<?php echo $landlords->user_id; ?>">
+                                                                        <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
+                                                                        <input type="submit" name="delete_landlord" value="Delete" class="text-center btn btn-danger">
                                                                     </div>
                                                                 </form>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <!-- End Modal -->
-
-                                                <!-- Delete Modal -->
-                                                <div class="modal fade" id="delete_" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                    <div class="modal-dialog modal-dialog-centered" role="document">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel">CONFIRM DELETE</h5>
-                                                                <button type="button" class="close" data-dismiss="modal">
-                                                                    <span>&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <form method="POST">
-                                                                <div class="modal-body text-center text-danger">
-                                                                    <h4>Delete </h4>
-                                                                    <br>
-                                                                    <!-- Hide This -->
-                                                                    <input type="hidden" name="user_id" value="">
-                                                                    <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
-                                                                    <input type="submit" name="delete_staff" value="Delete" class="text-center btn btn-danger">
-                                                                </div>
-                                                            </form>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <!-- End Modal -->
-                                            </tr>
-
+                                                    <!-- End Modal -->
+                                                </tr>
+                                            <?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
