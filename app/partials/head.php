57a86be81fe37921a98fdc86fbaaa96a30f29c11
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-11 16:18:32 +0300

    Intergrate Toastr

--- app/partials/head.php
+++ app/partials/head.php
@@ -20,4 +20,6 @@
     <!-- Data Tables CDN-->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.25/datatables.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.7.1/css/buttons.dataTables.min.css" />
+    <!-- Toastr -->
+    <link rel="stylesheet" href="../assets/plugins/toastr/toastr.min.css">
 </head>
