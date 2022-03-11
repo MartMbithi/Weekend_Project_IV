895690dd421ab5d18c7c0785c57c5b200a4391ae
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-11 15:58:52 +0300

    Implement Register Backend Logic

--- ui/register.php
+++ ui/register.php
@@ -1,5 +1,61 @@
 <?php
-/* Handle Login */
+session_start();
+require_once '../app/settings/config.php';
+require_once '../app/settings/codeGen.php';
+
+/* Handle Sign In */
+if (isset($_POST['create_account'])) {
+    $user_name =  $_POST['user_name'];
+    $user_idno = $_POST['user_idno'];
+    $user_email = $_POST['user_email'];
+    $new_password = sha1(md5($_POST['new_password']));
+    $confirm_password = sha1(md5($_POST['confirm_password']));
+    $user_phoneno = $_POST['user_phoneno'];
+
+    /* Check If Passwords Match */
+    if ($new_password != $confirm_password) {
+        $err = "Passwords Does Not Match";
+    } else {
+        /* Avoid Data Replication Exists */
+        $sql = "SELECT * FROM  users  
+    WHERE user_idno = '$user_idno' || user_phoneno = '$user_phoneno' || user_email = '$user_email'";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $users = mysqli_fetch_assoc($res);
+            /* If ID Number Already Exists Exit */
+            if (
+                $users['user_idno'] == $user_idno
+                || $users['user_phoneno'] == $user_phoneno
+                || $users['user_email'] == $user_email
+            ) {
+                $err = "National ID Number, Email Or Phone Number Already Exists";
+            }
+        } else {
+            /* Insert Details */
+            $sql = "INSERT INTO users (user_name, user_idno, user_email, user_password, user_phoneno, user_access_level)
+            VALUES(?,?,?,?,?,?)";
+            $prepare = $mysqli->prepare($sql);
+            $bind = $prepare->bind_param(
+                'ssssss',
+                $user_name,
+                $user_idno,
+                $user_email,
+                $user_password,
+                $user_phoneno,
+                $user_access_level
+            );
+            $prepare->execute();
+            if ($prepare) {
+                /* Pass This Alert Via Session */
+                $_SESSION['success'] = 'Your Account Has Been Created, Proceed To Login';
+                header('Location: index');
+                exit;
+            } else {
+                $err = "Failed!, Please Try Again";
+            }
+        }
+    }
+}
 require_once('../app/partials/head.php');
 ?>
 
@@ -34,6 +90,14 @@ require_once('../app/partials/head.php');
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="email" required name="user_idno" class="form-control" placeholder="National ID Number">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user-tag"></span>
                             </div>
                         </div>
                     </div>
