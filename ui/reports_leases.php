d429977db45e73602bd2e4a526806fc9cf97010d
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-14 05:52:57 +0300

    Implement Leasess Reports

--- ui/reports_leases.php
+++ ui/reports_leases.php
@@ -22,14 +22,14 @@ require_once('../app/partials/head.php');
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Property Reports</h1>
+                        <div class="col-sm-8">
+                            <h1 class="m-0 text-dark">Property Leases Reports</h1>
                         </div><!-- /.col -->
-                        <div class="col-sm-6">
+                        <div class="col-sm-4">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
                                 <li class="breadcrumb-item"><a href="">Reports</a></li>
-                                <li class="breadcrumb-item active">Properties</li>
+                                <li class="breadcrumb-item active">Leases</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -49,29 +49,43 @@ require_once('../app/partials/head.php');
                                     <table class="report_table">
                                         <thead>
                                             <tr>
-                                                <th>Code</th>
-                                                <th>Name</th>
-                                                <th>Category</th>
-                                                <th>Property Landlord</th>
-                                                <th>Location</th>
+                                                <th>Property Details</th>
+                                                <th>Tenant Details</th>
+                                                <th>Lease Agreement Details</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $ret = "SELECT * FROM properties p 
+                                            $ret = "SELECT * FROM property_leases pl
+                                            INNER JOIN  properties p on p.property_id = pl.lease_property_id
                                             INNER JOIN categories c ON c.category_id  = p.property_category_id
-                                            INNER JOIN users u ON u.user_id = p.property_landlord_id";
+                                            INNER JOIN users u ON u.user_id = pl.lease_tenant_id 
+                                            WHERE pl.lease_eviction_status = '0'
+                                            ";
                                             $stmt = $mysqli->prepare($ret);
                                             $stmt->execute(); //ok
                                             $res = $stmt->get_result();
-                                            while ($properties = $res->fetch_object()) {
+                                            while ($leases = $res->fetch_object()) {
                                             ?>
                                                 <tr>
-                                                    <td><?php echo $properties->property_code; ?></td>
-                                                    <td><?php echo $properties->property_name; ?></td>
-                                                    <td><?php echo $properties->category_name; ?></td>
-                                                    <td><?php echo $properties->user_name; ?></td>
-                                                    <td><?php echo $properties->property_address; ?></td>
+                                                    <td>
+                                                        <b>Code: </b> <?php echo $leases->property_code; ?> <br>
+                                                        <b>Name: </b> <?php echo $leases->property_name; ?> <br>
+                                                        <b>Category: </b> <?php echo $leases->category_name; ?> <br>
+                                                        <b>Location : </b> <?php echo $leases->property_address; ?>
+                                                    </td>
+                                                    <td>
+                                                        <b>Name: </b> <?php echo $leases->user_name; ?> <br>
+                                                        <b>IDNO: </b> <?php echo $leases->user_idno; ?> <br>
+                                                        <b>Phone No : </b> <?php echo $leases->user_phoneno; ?> <br>
+                                                        <b>Email : </b> <?php echo $leases->user_email; ?>
+                                                    </td>
+                                                    <td>
+                                                        <b>REF: </b> <?php echo $leases->lease_ref; ?> <br>
+                                                        <b>Duration: </b> <?php echo $leases->lease_duration; ?> Months <br>
+                                                        <b>Payment Status: </b> <?php echo $leases->lease_payment_status; ?> <br>
+                                                        <b>Date Leased: </b> <?php echo $leases->lease_date_added; ?>
+                                                    </td>
                                                 </tr>
                                             <?php } ?>
                                         </tbody>
