95212a423862076c7bb7b5049bc7a09e1b92ab96
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-15 06:42:44 +0300

    Implement Landlord Module

---
+++ ui/landlord_reports_leases.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+require_once('../app/settings/config.php');
+require_once('../app/settings/codeGen.php');
+require_once('../app/settings/checklogin.php');
+check_login();
+require_once('../app/partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('../app/partials/nav.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('../app/partials/aside.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-8">
+                            <h1 class="m-0 text-dark">Property Leases Reports</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-4">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="landlord_dashboard">Home</a></li>
+                                <li class="breadcrumb-item"><a href="">Reports</a></li>
+                                <li class="breadcrumb-item active">Leases</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+                <hr>
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Info boxes -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card card-warning card-outline">
+                                <div class="card-body">
+                                    <table class="report_table">
+                                        <thead>
+                                            <tr>
+                                                <th>Property Details</th>
+                                                <th>Tenant Details</th>
+                                                <th>Lease Agreement Details</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $user_id = $_SESSION['user_id'];
+                                            $ret = "SELECT * FROM property_leases pl
+                                            INNER JOIN  properties p on p.property_id = pl.lease_property_id
+                                            INNER JOIN categories c ON c.category_id  = p.property_category_id
+                                            INNER JOIN users u ON u.user_id = pl.lease_tenant_id 
+                                            WHERE pl.lease_eviction_status = '0' AND p.property_landlord_id = '$user_id'
+                                            ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($leases = $res->fetch_object()) {
+                                            ?>
+                                                <tr>
+                                                    <td>
+                                                        <b>Code: </b> <?php echo $leases->property_code; ?> <br>
+                                                        <b>Name: </b> <?php echo $leases->property_name; ?> <br>
+                                                        <b>Category: </b> <?php echo $leases->category_name; ?> <br>
+                                                        <b>Location : </b> <?php echo $leases->property_address; ?>
+                                                    </td>
+                                                    <td>
+                                                        <b>Name: </b> <?php echo $leases->user_name; ?> <br>
+                                                        <b>IDNO: </b> <?php echo $leases->user_idno; ?> <br>
+                                                        <b>Phone No : </b> <?php echo $leases->user_phoneno; ?> <br>
+                                                        <b>Email : </b> <?php echo $leases->user_email; ?>
+                                                    </td>
+                                                    <td>
+                                                        <b>REF: </b> <?php echo $leases->lease_ref; ?> <br>
+                                                        <b>Duration: </b> <?php echo $leases->lease_duration; ?> Months <br>
+                                                        <b>Payment Status: </b> <?php echo $leases->lease_payment_status; ?> <br>
+                                                        <b>Date Leased: </b> <?php echo $leases->lease_date_added; ?>
+                                                    </td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <!-- Main Footer -->
+        <?php require_once('../app/partials/footer.php'); ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- Scripts -->
+    <?php require_once('../app/partials/scripts.php'); ?>
+</body>
+
+</html>
