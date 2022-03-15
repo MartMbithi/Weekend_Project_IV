7fe00f38b3f3d4121e394355e7f2213dd0274618
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-15 06:01:58 +0300

    All Unit Tests Pass On LandLord Register Property

---
+++ ui/landlord_properties_add.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+require_once('../app/settings/config.php');
+require_once('../app/settings/codeGen.php');
+require_once('../app/settings/checklogin.php');
+check_login();
+
+/* Add Property */
+if (isset($_POST['add_property'])) {
+    $property_code = $_POST['property_code'];
+    $property_name = $_POST['property_name'];
+    $property_cost = $_POST['property_cost'];
+    $property_category_id = $_POST['property_category_id'];
+    $property_landlord_id = $_SESSION['user_id'];
+    $property_address = $_POST['property_address'];
+
+    /* Perisist */
+    $sql = "INSERT INTO  properties (property_code, property_name, property_cost, property_category_id, property_landlord_id, property_address)
+    VALUES(?,?,?,?,?,?)";
+    $prepare = $mysqli->prepare($sql);
+    $bind = $prepare->bind_param(
+        'ssssss',
+        $property_code,
+        $property_name,
+        $property_cost,
+        $property_category_id,
+        $property_landlord_id,
+        $property_address,
+    );
+    $prepare->execute();
+    if ($prepare) {
+        $success = "Rental Property Added";
+    } else {
+        $err = "Failed!, Please Try Again";
+    }
+}
+require_once('../app/partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('../app/partials/nav.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('../app/partials/aside.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-8">
+                            <h1 class="m-0 text-dark">Register Property</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-4">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="landlord_dashboard">Home</a></li>
+                                <li class="breadcrumb-item"><a href="">Properties</a></li>
+                                <li class="breadcrumb-item active">Register</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+                <hr>
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Info boxes -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card card-warning card-outline">
+                                <div class="card-body">
+                                    <form method="post" enctype="multipart/form-data" role="form">
+                                        <div class="row">
+                                            <div class="form-group col-md-12">
+                                                <label for="">Property Name</label>
+                                                <input type="text" required name="property_name" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Property Code</label>
+                                                <input type="text" readonly value="<?php echo $a . $b; ?>" required name="property_code" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Monthly Rent (Ksh)</label>
+                                                <input type="text" required name="property_cost" class="form-control">
+                                            </div>
+                                            <div class="form-group col-md-4">
+                                                <label for="">Property Category</label>
+                                                <select class="form-control basic" name="property_category_id">
+                                                    <option>Select Category</option>
+                                                    <?php
+                                                    $ret = "SELECT * FROM categories  ";
+                                                    $stmt = $mysqli->prepare($ret);
+                                                    $stmt->execute(); //ok
+                                                    $res = $stmt->get_result();
+                                                    while ($cat = $res->fetch_object()) {
+                                                    ?>
+                                                        <option value="<?php echo $cat->category_id; ?>"><?php echo $cat->category_code . ' - ' . $cat->category_name; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-md-12">
+                                                <label for="">Property Address</label>
+                                                <textarea type="text" name="property_address" class="form-control"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="text-right">
+                                            <button type="submit" name="add_property" class="btn btn-warning">Add Property</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <!-- Main Footer -->
+        <?php require_once('../app/partials/footer.php'); ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- Scripts -->
+    <?php require_once('../app/partials/scripts.php'); ?>
+</body>
+
+</html>
