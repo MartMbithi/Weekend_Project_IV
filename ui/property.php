ab905bb540d12b18253742569b7618cb18ffa6e0
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-24 18:34:43 +0300

    Implement Property Details UI

--- ui/property.php
+++ ui/property.php
@@ -14,8 +14,11 @@ require_once('../app/partials/head.php');
 
         <!-- Main Sidebar Container -->
         <?php require_once('../app/partials/aside.php');
-        $view = $_SESSION['view'];
-        $ret = "SELECT * FROM properties WHERE property_id = '$view' ";
+        $view = $_GET['view'];
+        $ret = "SELECT * FROM properties p 
+        INNER JOIN categories c ON c.category_id  = p.property_category_id
+        INNER JOIN users u ON u.user_id = p.property_landlord_id
+        WHERE property_id = '$view' ";
         $stmt = $mysqli->prepare($ret);
         $stmt->execute(); //ok
         $res = $stmt->get_result();
@@ -47,25 +50,77 @@ require_once('../app/partials/head.php');
                         <div class="row">
                             <div class="col-md-12">
                                 <!-- Profile Image -->
-                                <div class="card card-warning card-outline">
+                                <div class="card card-warning card-outline card-body">
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                        </ol>
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src="../data/<?php echo $property->property_img_1; ?>" class="d-block w-100" alt="...">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img src="../data/<?php echo $property->property_img_2; ?>" class="d-block w-100" alt="...">
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <!-- /.card -->
-
                             </div>
                             <!-- /.col -->
                             <div class="col-md-12">
                                 <div class="card card-warning card-outline">
                                     <div class="card-header p-2">
-                                        <ul class="nav nav-pills">
-                                            <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Property Details</a></li>
-                                            <li class="nav-item"><a class="nav-link" href="#caretaker" data-toggle="tab">Assigned Caretaker </a></li>
-                                            <li class="nav-item"><a class="nav-link" href="#lease" data-toggle="tab">Leases History </a></li>
-
-                                        </ul>
+                                        <h5 class="text-center">Property Details</h5>
                                     </div><!-- /.card-header -->
                                     <div class="card-body">
                                         <div class="tab-content">
                                             <div class="active tab-pane" id="activity">
+                                                <div class="row">
+                                                    <div class="col-6">
+                                                        <fieldset class="border border-primary p-2">
+                                                            <legend class="w-auto text-primary font-weight-light">Rental Property Details </legend>
+                                                            <div class="card-body">
+                                                                <ul class="list-group list-group-unbordered mb-3">
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-tag text-warning"></i> Code: </b> <a class="float-right"><?php echo $property->property_code; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-check text-warning"></i> Name: </b> <a class="float-right"><?php echo $property->property_name; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-list  text-warning"></i> Category: </b> <a class="float-right"><?php echo $property->category_name; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-map-pin text-warning"></i> Location: </b> <a class="float-right"><?php echo $property->property_address; ?></a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </fieldset>
+                                                    </div>
+                                                    <div class="col-6">
+                                                        <fieldset class="border border-primary p-2">
+                                                            <legend class="w-auto text-primary font-weight-light">Property Owner Details </legend>
+                                                            <div class="card-body">
+                                                                <ul class="list-group list-group-unbordered mb-3">
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-user-tie text-warning"></i> Names: </b> <a class="float-right"><?php echo $property->user_name; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-phone text-warning"></i> Contacts: </b> <a class="float-right"><?php echo $property->user_phoneno; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-envelope text-warning"></i> Email: </b> <a class="float-right"><?php echo $property->user_email; ?></a>
+                                                                    </li>
+                                                                    <li class="list-group-item">
+                                                                        <b><i class="fas fa-map-pin text-warning"></i> Address: </b> <a class="float-right"><?php echo $property->user_address; ?></a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </fieldset>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <!-- /.tab-pane -->
                                             <div class="tab-pane" id="caretaker">
