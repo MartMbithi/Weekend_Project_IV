ddddcc3faabe61e669ad061c032567ee5134f714
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-22 18:15:30 +0300

    Persist Database Changes On Rentals

--- ui/leases_manage.php
+++ ui/leases_manage.php
@@ -12,8 +12,8 @@ if (isset($_POST['evict'])) {
     $lease_property_id = $_POST['lease_property_id'];
 
     /* Persist */
-    $sql = "UPDATE property_leases SET lease_eviction_status =? WHERE lease_id =?";
-    $property_sql = "UPDATE properties SET property_status = 'Vacant' WHERE property_id ='$lease_property_id'";
+    $sql = "UPDATE house_rentals SET rental_eviction_status =? WHERE rental_id =?";
+    $property_sql = "UPDATE houses SET house_status = 'Vacant' WHERE house_id ='$lease_property_id'";
 
     $prepare = $mysqli->prepare($sql);
     $property_prepare = $mysqli->prepare($property_sql);
@@ -36,7 +36,7 @@ if (isset($_POST['update_lease'])) {
     $lease_duration = $_POST['lease_duration'];
 
     /* Persist */
-    $sql = "UPDATE property_leases SET  lease_duration =? WHERE lease_id =?";
+    $sql = "UPDATE house_rentals SET  rental_duration =? WHERE rental_id =?";
     $prepare = $mysqli->prepare($sql);
     $bind = $prepare->bind_param(
         'ss',
@@ -57,8 +57,8 @@ if (isset($_POST['delete_lease'])) {
     $lease_property_id = $_POST['lease_property_id'];
 
     /* Delete */
-    $sql = "DELETE FROM property_leases WHERE lease_id =?";
-    $property_sql = "UPDATE properties SET property_status = 'Vacant' WHERE property_id ='$lease_property_id'";
+    $sql = "DELETE FROM house_rentals WHERE rental_id =?";
+    $property_sql = "UPDATE houses SET house_status = 'Vacant' WHERE house_id ='$lease_property_id'";
 
 
     $prepare = $mysqli->prepare($sql);
@@ -128,11 +128,11 @@ require_once('../app/partials/head.php');
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $ret = "SELECT * FROM property_leases pl
-                                            INNER JOIN  properties p on p.property_id = pl.lease_property_id
-                                            INNER JOIN categories c ON c.category_id  = p.property_category_id
-                                            INNER JOIN users u ON u.user_id = pl.lease_tenant_id 
-                                            WHERE pl.lease_eviction_status = '0'
+                                            $ret = "SELECT * FROM house_rentals hr
+                                            INNER JOIN  houses h on h.house_id = hr.rental_house_id
+                                            INNER JOIN categories c ON c.category_id  = h.house_category_id
+                                            INNER JOIN users u ON u.user_id = hr.rental_tenant_id 
+                                            WHERE hr.rental_eviction_status = '0'
                                             ";
                                             $stmt = $mysqli->prepare($ret);
                                             $stmt->execute(); //ok
@@ -141,10 +141,10 @@ require_once('../app/partials/head.php');
                                             ?>
                                                 <tr>
                                                     <td>
-                                                        <b>Code: </b> <?php echo $leases->property_code; ?> <br>
-                                                        <b>Name: </b> <?php echo $leases->property_name; ?> <br>
+                                                        <b>Code: </b> <?php echo $leases->house_code; ?> <br>
+                                                        <b>Name: </b> <?php echo $leases->house_name; ?> <br>
                                                         <b>Category: </b> <?php echo $leases->category_name; ?> <br>
-                                                        <b>Location : </b> <?php echo $leases->property_address; ?>
+                                                        <b>Location : </b> <?php echo $leases->house_address; ?>
                                                     </td>
                                                     <td>
                                                         <b>Name: </b> <?php echo $leases->user_name; ?> <br>
@@ -153,23 +153,23 @@ require_once('../app/partials/head.php');
                                                         <b>Email : </b> <?php echo $leases->user_email; ?>
                                                     </td>
                                                     <td>
-                                                        <b>REF: </b> <?php echo $leases->lease_ref; ?> <br>
-                                                        <b>Duration: </b> <?php echo $leases->lease_duration; ?> Months <br>
-                                                        <b>Payment Status: </b> <?php echo $leases->lease_payment_status; ?> <br>
-                                                        <b>Date: </b> <?php echo $leases->lease_date_added; ?>
+                                                        <b>REF: </b> <?php echo $leases->rental_ref; ?> <br>
+                                                        <b>Duration: </b> <?php echo $leases->rental_duration; ?> Months <br>
+                                                        <b>Payment Status: </b> <?php echo $leases->rental_payment_status; ?> <br>
+                                                        <b>Date: </b> <?php echo $leases->rental_date_added; ?>
                                                     </td>
                                                     <td>
-                                                        <a data-toggle="modal" href="#update_<?php echo $leases->lease_id; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a> <br>
-                                                        <a data-toggle="modal" href="#vacate_<?php echo $leases->lease_id; ?>" class="badge badge-warning"><i class="fas fa-ban"></i> Evict</a> <br>
-                                                        <a data-toggle="modal" href="#delete_<?php echo $leases->lease_id; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
+                                                        <a data-toggle="modal" href="#update_<?php echo $leases->rental_id; ?>" class="badge badge-primary"><i class="fas fa-edit"></i> Edit</a> <br>
+                                                        <a data-toggle="modal" href="#vacate_<?php echo $leases->rental_id; ?>" class="badge badge-warning"><i class="fas fa-ban"></i> Evict</a> <br>
+                                                        <a data-toggle="modal" href="#delete_<?php echo $leases->rental_id; ?>" class="badge badge-danger"><i class="fas fa-trash"></i> Delete</a>
                                                     </td>
                                                     <!-- Update Modal -->
-                                                    <div class="modal fade fixed-right" id="update_<?php echo $leases->lease_id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+                                                    <div class="modal fade fixed-right" id="update_<?php echo $leases->rental_id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
                                                         <div class="modal-dialog  modal-xl" role="document">
                                                             <div class="modal-content">
                                                                 <div class="modal-header align-items-center">
                                                                     <div class="text-bold">
-                                                                        <h6 class="text-bold">Update <?php echo $leases->lease_ref; ?> </h6>
+                                                                        <h6 class="text-bold">Update <?php echo $leases->rental_ref; ?> </h6>
                                                                     </div>
                                                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                                         <span aria-hidden="true">&times;</span>
@@ -181,17 +181,17 @@ require_once('../app/partials/head.php');
                                                                             <div class="form-group col-md-9">
                                                                                 <label for="">House Details</label>
                                                                                 <!-- Hide This -->
-                                                                                <input type="hidden" required name="lease_id" value="<?php echo $leases->lease_id; ?>" class="form-control">
+                                                                                <input type="hidden" required name="lease_id" value="<?php echo $leases->rental_id; ?>" class="form-control">
                                                                                 <select class="form-control basic" name="lease_property_id">
-                                                                                    <option value="<?php echo $leases->lease_property_id; ?>">
-                                                                                        Code: <?php echo $leases->property_code . ', Name: ' . $leases->property_name . ', Location: ' . $leases->property_address; ?>
+                                                                                    <option value="<?php echo $leases->rental_house_id; ?>">
+                                                                                        Code: <?php echo $leases->house_code . ', Name: ' . $leases->house_name . ', Location: ' . $leases->house_address; ?>
                                                                                     </option>
                                                                                 </select>
                                                                             </div>
                                                                             <div class="form-group col-md-3">
                                                                                 <label for="">Rental Duration (Months)</label>
                                                                                 <select class="form-control basic" name="lease_duration">
-                                                                                    <option><?php echo $leases->lease_duration; ?></option>
+                                                                                    <option><?php echo $leases->rental_duration; ?></option>
                                                                                     <option>1</option>
                                                                                     <option>2</option>
                                                                                     <option>3</option>
@@ -218,7 +218,7 @@ require_once('../app/partials/head.php');
                                                     <!-- End Modal -->
 
                                                     <!-- Vacate Modal -->
-                                                    <div class="modal fade" id="vacate_<?php echo $leases->lease_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal fade" id="vacate_<?php echo $leases->rental_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                         <div class="modal-dialog modal-dialog-centered" role="document">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
@@ -232,8 +232,8 @@ require_once('../app/partials/head.php');
                                                                         <h4>Evict Tenant?</h4>
                                                                         <br>
                                                                         <!-- Hide This -->
-                                                                        <input type="hidden" name="lease_id" value="<?php echo $leases->lease_id; ?>">
-                                                                        <input type="hidden" name="lease_property_id" value="<?php echo $leases->lease_property_id; ?>">
+                                                                        <input type="hidden" name="lease_id" value="<?php echo $leases->rental_id; ?>">
+                                                                        <input type="hidden" name="lease_property_id" value="<?php echo $leases->rental_house_id; ?>">
                                                                         <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
                                                                         <input type="submit" name="evict" value="Yes" class="text-center btn btn-danger">
                                                                     </div>
@@ -242,7 +242,7 @@ require_once('../app/partials/head.php');
                                                         </div>
                                                     </div>
                                                     <!-- Delete Modal -->
-                                                    <div class="modal fade" id="delete_<?php echo $leases->lease_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal fade" id="delete_<?php echo $leases->rental_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                                         <div class="modal-dialog modal-dialog-centered" role="document">
                                                             <div class="modal-content">
                                                                 <div class="modal-header">
@@ -256,8 +256,8 @@ require_once('../app/partials/head.php');
                                                                         <h4>Delete?</h4>
                                                                         <br>
                                                                         <!-- Hide This -->
-                                                                        <input type="hidden" name="lease_id" value="<?php echo $leases->lease_id; ?>">
-                                                                        <input type="hidden" name="lease_property_id" value="<?php echo $leases->lease_property_id; ?>">
+                                                                        <input type="hidden" name="lease_id" value="<?php echo $leases->rental_id; ?>">
+                                                                        <input type="hidden" name="lease_property_id" value="<?php echo $leases->rental_house_id; ?>">
                                                                         <button type="button" class="text-center btn btn-success" data-dismiss="modal">No</button>
                                                                         <input type="submit" name="delete_lease" value="Delete" class="text-center btn btn-danger">
                                                                     </div>
